aa7e9781119b7c23f2663d219c4bce1d167bfc03
caihe <caihebatista@gmail.com>
2023-07-23 20:40:33 -0300

    initial

---
+++ src/Texts/ChapterOne.java
@@ -0,0 +1,61 @@
+
+package Texts;
+import InputVariables.GetName;
+public class ChapterOne {
+    public static String[] initialBlockOne() {
+        String[] data = {
+    "\tApesar do clima tenso da situação anterior, você aceita a mão estendida do misterioso mago e se levanta com sua ajuda. A adrenalina da perseguição havia ofuscado a dor, mas agora você percebe que se feriu ao tropeçar na fuga. A dor se torna mais intensa, e suas pernas tremem levemente.",
+    "\tO mago observa seu estado e percebe sua hesitação. Com uma voz calma e reconfortante, ele diz, enquanto estende sua mão.",
+    "- Não se preocupe, você está bem.",
+    "\tVocê aperta a mão dele e se ergue do chão, tirando a poeira de sua roupa.",
+    "- Valeu mesmo, eu achei que ia acabar aqui.",
+    "- Essa floresta está repleta de lobos. Com o caos, eles se assustaram e se afastaram do reino.",
+    "- Caos?",
+    "\tO homem te olha com um olhar surpreso.",
+    "- Longa história. Suponho que não frequenta muito o reino.",
+    "- Não. Eu moro por aqui.",
+    "- Porque estava por essa floresta, então?",
+    "- Estou atrás de meu pai... Ele já era pra ter voltado - Você responde, de cabeça baixa.",
+    "\tO homem olha suas pernas e nota suas feridas.",
+    "- Você não vai conseguir seguir com essas feridas. Venha comigo e eu te ajudo a sará-las.",
+    "\tVocê olha para o mago, ainda com um pouco de desconfiança. Entretanto, ele acabou de te salvar... E também você sabe que qualquer ajuda é bem vinda nesse momento.",
+    "- Beleza. ",
+    "- Sou Edidor, e você?",
+    "- " + GetName.name + ". " + GetName.name + "Ednack.",
+    "\tErion te olha com um olhar estranho, mas olha pra frente e fala:",
+    "- Vamos. Ou vai acabar inflamando."
+        };
+        return data;
+    }
+    public static String[] secondBlockOne() {
+       String[] data = {
+        "\tDepois de uma breve caminhada, vocês finalmente chegam ao covil, e ele te aponta uma pedra onde você pode se sentar e descansar sua perna.",
+        "- Fique ali, vou preparar umas poções para você.",
+        "- ...Poções?",
+        "É. Elas vão acelerar a sua regeneração e curar suas feridas.",
+        "\tNaquele momento, toda aquela desconfiança volta em dobro, afinal uma poção tão milagrosa assim? Seu pai sempre disse que magia e alquimia era como maldições..."   
+            };
+       return data;
+    }
+    public static String[] thirdBlockOne() {
+        String[] data = {
+            "- Então... Por onde seu pai era pra estar?",
+            "- Pelo reino. Ele é um soldado importante do exército, e tinha sido chamado urgentemente.",
+            "- Entendo. Não se espante, tentarei ser direto. O reino está em guerra e o orbe da luz, um poderossísimo objeto, foi roubado, o que desencadeou o caos. Monstros estão por toda parte...",
+            "- Mas... Pessoas morreram?",
+            "- Cidadões despreparados. Aqueles que tivessem alguma defesa conseguiram pelo menos sair com vida de ataques. Os soldados do reino agiram rápido, e por mais que não fossem treinados para atacar monstros, eles se viraram bem. Mas quanto mais eles se defendem, mais monstros aparecem... A tendêcia é piorar.",
+            "- Droga... Mas meu pai era um dos melhores do reino. Eu sei que ele vai aguentar! Mas eu preciso ser rápido.",
+            "- Vai ser difícil o achar em meio a toda essa bagunça. Precisamos cortar o mal pela raiz.",
+            "- Como... Como assim?",
+            "- Eu conheço um minério que pode gerar um poder tão potente quanto o do orbe. A pedra cromática. Mas não conseguirei obté-la sozinho. Você pode me ajudar?",
+            "\tVocê pensa e não consegue achar um motivo pra recusar. E também isso parece ser bem mais agradável que tentar entrar num reino em guerra",
+            "- Sim. Eu vou te ajudar!",
+            "- Ótimo. Mas antes, você precisa comprar uma espada que preste.",
+            "- Mas... Onde eu vou comprar uma?",
+            "- Há alguns vilarejos do outro lado da floresta. Talvez tenha algo de interessante por lá, dê uma olhada.",
+            "- E você?",
+            "Eu vou ficar por aqui e tentar remendar meu cajado. Fique tranquilo que essa hora os lobos já devem estar alimentados.",
+            "\tVocê acena com a cabeça e vai rumo a saída do covil.",};
+        return data;
+    }
+}
